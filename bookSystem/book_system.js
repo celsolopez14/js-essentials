884ab4c44c79c18749d7f8a1049eb898fabcb1ab
Celso <celopez@my.bridgeport.edu>
2024-01-14 15:26:51 -0500

    Objects Practice

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -26,7 +26,8 @@ function showbooks(){
     <P><strong>Book Name: </strong>${book.name}</P>
       <p><strong>Author Name:</strong> ${book.authorName} </p>
       <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-      <p><strong>No. of Pages:</strong> ${book.pagesNumber} mins</p>`);
+      <p><strong>No. of Pages:</strong> ${book.pagesNumber} mins</p>
+      <button onclick="deleteBook(${index})">Delete Book</button>`);
       document.getElementById('books').innerHTML = booksDiv;
 }
 
@@ -35,4 +36,9 @@ function clearInputs() {
     document.getElementById('authorName').value = '';
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
+}
+
+function deleteBook(id){
+    books.splice(id,1);
+    showbooks();
 }
